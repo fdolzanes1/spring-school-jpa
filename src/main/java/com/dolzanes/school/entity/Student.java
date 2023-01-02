19ca2fed97a75aa8da6f0618a1f526865681fbaa
FabianoDolzanes <fdolzanes1@gmail.com>
2023-01-02 12:40:40 -0400

    Added new annotations

--- src/main/java/com/dolzanes/school/entity/Student.java
+++ src/main/java/com/dolzanes/school/entity/Student.java
@@ -1,9 +1,13 @@
 package com.dolzanes.school.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,13 +19,33 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+		name="tbl_students",
+		uniqueConstraints = @UniqueConstraint(
+				name="emailid_unique",
+				columnNames = "email_address"
+		)
+)
 public class Student {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(
+			name = "student_sequence",
+			sequenceName = "student_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE, 
+			generator = "student_sequence"
+	)
 	private Long studentId;
 	private String firstName;
 	private String lastName;
+	
+	@Column(
+			name="email_address",
+			nullable = false
+	)
 	private String emailId;
 	private String guardianName;
 	private String guardianEmail;
