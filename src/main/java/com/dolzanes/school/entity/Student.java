d14084794f6946b13806cb498e8ccca5e07e3668
FabianoDolzanes <fdolzanes1@gmail.com>
2023-01-02 12:17:59 -0400

    mapping Student class

--- src/main/java/com/dolzanes/school/entity/Student.java
+++ src/main/java/com/dolzanes/school/entity/Student.java
@@ -1,7 +1,29 @@
 package com.dolzanes.school.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Student {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long studentId;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String guardianName;
+	private String guardianEmail;
+	private String guardianMobile;
 }
