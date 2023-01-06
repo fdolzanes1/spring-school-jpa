f24ecbd96e047f74fbaa5a07b25072fd166b5721
FabianoDolzanes <fdolzanes1@gmail.com>
2023-01-05 22:44:59 -0400

    Create Course and CourseMaterial Model

---
+++ src/main/java/com/dolzanes/school/entity/Course.java
@@ -0,0 +1,32 @@
+package com.dolzanes.school.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+	
+	@Id
+	@SequenceGenerator(
+			name = "course_sequence",
+			sequenceName = "course_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long courseId;
+	private String title;
+	private Integer credit;
+	
+}
